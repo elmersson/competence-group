280a2aa3283e16c4cff721c04fdde0859e13e372
RasmusElmersson <126062642+RasmusElmersson@users.noreply.github.com>
2024-10-20 09:40:42 +0200

    feat: sidebar

--- src/app/(auth)/home/page.tsx
+++ src/app/(auth)/home/page.tsx
@@ -1,34 +1,3 @@
-import { SignOutButton, UserButton } from "@clerk/nextjs"
-
 export default async function HomePage() {
-    return (
-        <>
-            <main className="max-w-[75rem] w-full mx-auto">
-                <div className="grid grid-cols-[1fr_20.5rem] gap-10 pb-10">
-                    <div>
-                        <header className="flex items-center justify-between w-full h-16 gap-4">
-                            <div className="flex items-center gap-2">
-                                <UserButton
-                                    afterSignOutUrl="/"
-                                    appearance={{
-                                        elements: {
-                                            userButtonAvatarBox: "size-6",
-                                        },
-                                    }}
-                                />
-                                <SignOutButton>
-                                    <button
-                                        type="button"
-                                        className="text-sm font-semibold text-white bg-red-500 rounded px-4 py-2"
-                                    >
-                                        Sign Out
-                                    </button>
-                                </SignOutButton>
-                            </div>
-                        </header>
-                    </div>
-                </div>
-            </main>
-        </>
-    )
+    return <div>Home</div>
 }
